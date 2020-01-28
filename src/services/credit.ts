e308d172f3e0a0da530cb7065f47e075badf47c1
Enrique Martín García <enrique@emartin.dev>
2020-01-28 15:04:19 +0100

    Service fully functional

--- src/services/credit.ts
+++ src/services/credit.ts
@@ -4,18 +4,30 @@ export interface ICreditCardsEndpoint {
   fetchAvailableCreditCards: (
     income: number,
     occupation: string,
-  ) => Promise<void>
+  ) => Promise<ICreditCard[]>
+}
+
+export type ICreditCard = {
+  name: string
+  apr: number
+  balanceTransferOfferDuration: number
+  purchaseOfferDuration: number
+  creditAvailable: number
 }
 
 export const creditCardsEndPoint: ICreditCardsEndpoint = {
   fetchAvailableCreditCards: async (
     income: number,
     occupation: string,
-  ): Promise<void> => {
+  ): Promise<ICreditCard[]> => {
     const response = await axios.get(
       `http://localhost:4321/cards?income=${income}&occupation=${occupation}`,
     )
 
-    console.log(response)
+    if (response.status !== 200) {
+      throw new Error()
+    }
+
+    return response.data
   },
 }

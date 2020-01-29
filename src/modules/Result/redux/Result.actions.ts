6f05a7a4183239eeb4e44b325d1b9d4e5b97a96e
Enrique Martín García <enrique@emartin.dev>
2020-01-29 23:14:18 +0100

    Added card selection logic

--- src/modules/Result/redux/Result.actions.ts
+++ src/modules/Result/redux/Result.actions.ts
@@ -2,6 +2,8 @@ import { ICreditCard } from '../../../services/credit'
 
 export const SET_RESULT_LOADING = 'SET_RESULT_LOADING'
 export const SET_RESULT_DATA = 'SET_RESULT_DATA'
+export const SELECT_RESULT_CARD = 'SELECT_RESULT_CARD'
+export const DESELECT_RESULT_CARD = 'DESELECT_RESULT_CARD'
 
 interface ISetResultLoading {
   type: typeof SET_RESULT_LOADING
@@ -21,4 +23,28 @@ export const setResultData = (data: ICreditCard[]): ISetResultData => ({
   payload: data,
 })
 
-export type ResultActions = ISetResultLoading | ISetResultData
+interface ISelectResultCard {
+  type: typeof SELECT_RESULT_CARD
+  payload: number
+}
+
+export const selectResultCard = (id: number): ISelectResultCard => ({
+  type: SELECT_RESULT_CARD,
+  payload: id,
+})
+
+interface IDeselectResultCard {
+  type: typeof DESELECT_RESULT_CARD
+  payload: number
+}
+
+export const deselectResultCard = (id: number): IDeselectResultCard => ({
+  type: DESELECT_RESULT_CARD,
+  payload: id,
+})
+
+export type ResultActions =
+  | ISetResultLoading
+  | ISetResultData
+  | ISelectResultCard
+  | IDeselectResultCard
--- src/modules/Result/redux/Result.reducer.ts
+++ src/modules/Result/redux/Result.reducer.ts
@@ -4,21 +4,25 @@ import {
   ResultActions,
   SET_RESULT_LOADING,
   SET_RESULT_DATA,
+  SELECT_RESULT_CARD,
+  DESELECT_RESULT_CARD,
 } from './Result.actions'
 import { ICreditCard } from '../../../services/credit'
 
 type CreditCardsById = { [id: number]: ICreditCard }
 
-interface IResultReducerState {
+export interface IResultReducerState {
   isLoading: boolean
   cards: CreditCardsById
   cardIds: number[]
+  selectedCardIds: number[]
 }
 
-const INITIAL_STATE: IResultReducerState = {
+export const INITIAL_STATE: IResultReducerState = {
   isLoading: false,
   cards: {},
   cardIds: [],
+  selectedCardIds: [],
 }
 
 export const resultReducer: Reducer<IResultReducerState, ResultActions> = (
@@ -31,11 +35,37 @@ export const resultReducer: Reducer<IResultReducerState, ResultActions> = (
         draft.isLoading = true
         break
 
+      case SELECT_RESULT_CARD:
+        const id = action.payload
+        if (draft.selectedCardIds.includes(id)) {
+          throw new Error('Attempting to select a card twice')
+        }
+        draft.selectedCardIds.push(id)
+        break
+
+      case DESELECT_RESULT_CARD:
+        if (
+          !draft.selectedCardIds.length ||
+          !draft.selectedCardIds.includes(action.payload)
+        ) {
+          throw new Error('Attempted to remove a nonexisting element')
+        }
+        // TODO use splice
+        draft.selectedCardIds = draft.selectedCardIds.filter(
+          id => id !== action.payload,
+        )
+        break
+
       case SET_RESULT_DATA:
         const payload = action.payload
 
+        if (!payload.length) {
+          draft.isLoading = false
+          break
+        }
+
         const cardIds = payload.map(card => card.id)
-        const cards = indexById(payload)
+        const cards = indexById(action.payload)
 
         draft.cards = cards
         draft.cardIds = cardIds

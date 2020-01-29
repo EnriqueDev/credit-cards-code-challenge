0858e1aeeb8f55e70247cad79d263d3185e65057
Enrique Martín García <enrique@emartin.dev>
2020-01-29 23:14:01 +0100

    updated jest config to just allow .test.js extension

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
+  testRegex: '(/__tests__/.*\\.(test|spec))\\.(ts|tsx|js)$',
+  testPathIgnorePatterns: ['/node_modules/', '/build/'],
+
   testEnvironment: 'jsdom',
 }
